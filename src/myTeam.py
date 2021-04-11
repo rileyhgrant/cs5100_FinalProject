b72df7fe6ccab9810d50a1d8feeb1aa5da9e01b9
EgorPlay1 <56142881+EgorPlay1@users.noreply.github.com>
2021-04-11 18:48:25 -0400

    coding session results for 04/11/2021
    mainly worked on implementing the MCTS algo in myTeam.py

--- src/myTeam.py
+++ src/myTeam.py
@@ -16,14 +16,17 @@ from captureAgents import CaptureAgent
 import random, time, util
 from game import Directions
 import game
+import time
+import math
+
 
 #################
-# Team creation #
+# Team    wip   #
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'DummyAgent', second = 'DummyAgent'):
-  """
+               first='DummyAgent', second='DummyAgent'):
+    """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
   index numbers.  isRed is True if the red team is being created, and
@@ -38,22 +41,24 @@ def createTeam(firstIndex, secondIndex, isRed,
   behavior is what you want for the nightly contest.
   """
 
-  # The following line is an example only; feel free to change it.
-  return [eval(first)(firstIndex), eval(second)(secondIndex)]
+    # The following line is an example only; feel free to change it.
+    return [eval(first)(firstIndex), eval(second)(secondIndex)]
+
 
 ##########
 # Agents #
 ##########
 
-class DummyAgent(CaptureAgent):
-  """
+
+class ReflexCaptureAgent(CaptureAgent):
+    """
   A Dummy agent to serve as an example of the necessary agent structure.
   You should look at baselineTeam.py for more details about how to
   create an agent as this is the bare minimum.
   """
 
-  def registerInitialState(self, gameState):
-    """
+    def registerInitialState(self, gameState):
+        """
     This method handles the initial setup of the
     agent to populate useful fields (such as what team
     we're on).
@@ -65,28 +70,185 @@ class DummyAgent(CaptureAgent):
     IMPORTANT: This method may run for at most 15 seconds.
     """
 
-    '''
+        '''
     Make sure you do not delete the following line. If you would like to
     use Manhattan distances instead of maze distances in order to save
     on initialization time, please take a look at
     CaptureAgent.registerInitialState in captureAgents.py.
     '''
-    CaptureAgent.registerInitialState(self, gameState)
+        CaptureAgent.registerInitialState(self, gameState)
 
-    '''
+        '''
     Your initialization code goes here, if you need any.
     '''
 
-
-  def chooseAction(self, gameState):
-    """
+    def chooseAction(self, gameState):
+        """
     Picks among actions randomly.
     """
-    actions = gameState.getLegalActions(self.index)
+        actions = gameState.getLegalActions(self.index)
 
-    '''
+        '''
     You should change this in your own agent.
     '''
+        return random.choice(actions)
 
-    return random.choice(actions)
+    def evaluate(self, gameState, action):
+        """
+    Computes a linear combination of features and feature weights
+    """
+        features = self.getFeatures(gameState, action)
+        weights = self.getWeights(gameState, action)
+        return features * weights
 
+
+class OffensiveReflexAgent(ReflexCaptureAgent):
+
+    def __init__(self, index):
+        CaptureAgent.__init__(self, index)
+
+    def registerInitialState(self, gameState):
+        CaptureAgent.registerInitialState(self, gameState)
+
+    def chooseAction(self, gameState):
+        # have if statements to override the monte-carlo's choice
+        
+        def evaluate(MCNode):
+            
+            
+        mctsNode = MCTS( MCNode( gameState, self.index, None, evaluate))
+        return mctsNode.action
+        #return random.choice(actions)
+
+
+class DefensiveReflexAgent(ReflexCaptureAgent):
+
+    def registerInitialState(self, gameState):
+        CaptureAgent.registerInitialState(self, gameState)
+
+    def chooseAction(self, gameState):
+        # have if statements to override the monte-carlo's choice
+        def evaluate(MCNode):
+
+        mctsNode = MCTS( MCNode( gameState, self.index, None, evaluate ))
+        return mctsNode.action
+        #return random.choice(actions)
+
+
+# https://www.geeksforgeeks.org/ml-monte-carlo-tree-search-mcts/
+# https://medium.com/@quasimik/implementing-monte-carlo-tree-search-in-node-js-5f07595104df
+'''
+Class to represent the Monte Carlo Tree Node and its children
+'''
+
+
+class MCNode():
+    def __init__(self, gameState, index, action, evaluate):
+        self.numWins = 0
+        self.numGames = 0
+        self.gameState = gameState
+        self.parent = None
+        self.children = list()  # list of MCNodes
+        self.isEnemy = False
+        self.index = index
+        self.action = action
+        self.evaluate = evaluate
+
+    def isFullyExpanded(self):
+        for child in self.children:
+            if not child.visited():
+                return False
+
+        return True
+
+    def visited(self):
+        return self.numGames > 0
+
+    def getNodeWithBestUCT(self):
+        chosenNode = None
+        maxUCT = -1000000
+        for child in self.children:
+            if child.getUCT() > maxUCT:
+                maxUCT = child.getUCT()
+                chosenNode = child
+
+        return chosenNode
+
+    def getUCT(self):
+        exploreParam = math.sqrt(2)
+        return self.numWins / self.numGames + exploreParam * math.sqrt(math.log(self.parent.numGames) / self.numGames)
+
+    def hasChildren(self):
+        return len(self.children) != 0
+
+    def generateSuccessor(self):
+        if self.isFullyExpanded():
+            return random.choice(self.children)
+        else:
+            gameState = self.gameState.generateSuccessor(self.index, Directions.STOP)
+            successor = MCNode(gameState, gameState.getIndex(), Directions.STOP)
+            while not self.children.__contains__(successor):
+                action = random.choice(self.gameState.getLegalActions(self.index))
+                gameState = self.gameState.generateSuccessor(self.index, action)
+                successor = MCNode(gameState, gameState.getIndex(), action)
+            return successor
+
+
+def MCTS(MCNode):
+    start = time.time()
+    while 3000 > (time.time() - start) * 1000:  # resources remaining (time) 3s
+        leaf = search(MCNode)
+        result = rollout(leaf)
+        backpropagate(MCNode, result)
+
+    return best_child(MCNode)
+
+
+def best_child(MCNode):
+    maxWinrate = 0
+    bestChild = None
+    for child in MCNode.children:
+        winrate = child.numWins / child.numGames
+        if winrate > maxWinrate:
+            bestChild = child
+    return bestChild
+
+
+
+def search(MCNode):
+    currentNode = MCNode
+    while currentNode.isFullyExpanded():
+        currentNode = currentNode.getNodeWithBestUCT()
+
+    if currentNode.hasChildren():
+        for child in currentNode.children:
+            if not child.visited:
+                return child
+    else:
+        return currentNode
+
+
+# keep choosing random actions until an end state is reached
+def rollout(MCNode):
+    # potentially add depth parameter if time is an issue
+    currentNode = MCNode
+    while not currentNode.isTerminal():
+        currentNode = rollout_policy(currentNode)
+    return self.evaluate(currentNode)
+
+
+def rollout_policy(MCNode):
+    return MCNode.generateSuccessor()
+
+
+#
+def backpropagate(MCNode, result):
+    isEnemyWin = result < 0
+    currentNode = MCNode
+    while currentNode is not None:
+        currentNode.numPlays += 1
+        if (currentNode.isEnemy == isEnemyWin):
+            currentNode.numWins += 1
+
+        # set node to parent to keep looping
+        currentNode = currentNode.parent
